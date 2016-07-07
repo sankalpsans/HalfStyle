417a9ef733872ac0020adffab41b3a33c9fee979
Sankalp <sankalp.sans@gmail.com>
2016-07-08 00:38:17 +0530

    Removed jQuery dependency completely

--- js/halfstyle.js
+++ js/halfstyle.js
@@ -3,32 +3,26 @@
  * Copyright 2014 Arbel Hakopian
  * Licensed under MIT (https://github.com/arbelh/HalfStyle/blob/master/license.md)
  */
-jQuery(function($) {
+window.onload = function(){ initHalfStyle(); };
+function initHalfStyle() {
     var halfstyle_text, halfstyle_chars, $halfstyle_el, halfstyle_i, halfstyle_output, halfstyle_style;
 
     // Iterate over all class occurrences
     var domsToIterate = document.getElementsByClassName('textToHalfStyle');
     for(var i = 0; i < domsToIterate.length; i = i + 1) {
         halfstyle_el = domsToIterate[i];
-        $halfstyle_el = $(halfstyle_el);
-        console.log(halfstyle_el.attributes);
         halfstyle_style = halfstyle_el.attributes['data-halfstyle'].value;
         halfstyle_text = halfstyle_el.innerText;
         halfstyle_chars = halfstyle_text.split('');
 
         // Set the screen-reader text
-        halfstyle_el.innerHtml = '<span style="position: absolute !important;clip: rect(1px 1px 1px 1px);clip: rect(1px, 1px, 1px, 1px);">' + halfstyle_text + '</span>';
-
-        // Reset output for appending
-        halfstyle_output = '';
+	halfstyle_output = '<span style="position: absolute !important;clip: rect(1px 1px 1px 1px);clip: rect(1px, 1px, 1px, 1px);">' + halfstyle_text + '</span>';
 
         // Iterate over all chars in the text
         for (halfstyle_i = 0; halfstyle_i < halfstyle_chars.length; halfstyle_i++) {
             // Create a styled element for each character and append to container
             halfstyle_output += '<span aria-hidden="true" class="halfStyle ' + halfstyle_style + '" data-content="' + halfstyle_chars[halfstyle_i] + '">' + halfstyle_chars[halfstyle_i] + '</span>';
         }
-
-        // Write to DOM only once
-        $halfstyle_el.append(halfstyle_output);
+	halfstyle_el.innerHTML = halfstyle_output;
     }
-});
+};
